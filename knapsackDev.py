0bbfe03d92767ac6ec54d6c2e9b7707fa9d18137
Loubens Louis <53797568+llouis01@users.noreply.github.com>
2021-02-20 22:59:03 -0500

    knapsack algorithm

--- knapsackDev.py
+++ knapsackDev.py
@@ -5,7 +5,8 @@ import json
 
 def checkCapacity(contents,knapsack_cap):
     """ contents is expected as a dictionaryof the form {item_id:(volume,value), ...} """
-    """ This function returns True if the knapsack is within capacity; False if the knapsack is overloaded """
+    """ This function returns True if the knapsack is within capacity;
+        False if the knapsack is overloaded """
     load = 0
     if isinstance(contents,dict):
         for this_key in contents.keys():
@@ -38,10 +39,13 @@ def getData():
     return x
 
 def loadKnapsack(items,knapsack_cap):
-    """ You write this function which is your heuristic knapsack algorithm
+    """ You write this function which is your heuristic
+        knapsack algorithm
     
-        Indicate items to be included in the backpack by including their dictionary keys within 
-        a list data structure and, subsequently, returning that list of IDs from this function  """
+        Indicate items to be included in the backpack by
+        including their dictionary keys within a list
+        data structure and, subsequently, returning that
+        list of IDs from this function  """
         
     """ Compute existing load in knapsack """
     myUsername = 'llouis' # always return this variable as the first item
@@ -50,15 +54,47 @@ def loadKnapsack(items,knapsack_cap):
     
     load = 0.0            # use this variable to keep track of how much volume is already loaded into the backpack
     value = 0.0           # value in knapsack
+    
+    
+    """
+    Step 1: as usual for i in range(len(my_data)):
+
+    Step 2: if the weight of the items you index from
+            "my_data" is < knapsack_cap - the items you've added then load into the pack
+
+    my_data(then do your indexing) < knapsack_cap
+    load = load + my_data(with your indexing)
+
+    Step 3: append my_data[i][0] (b/c we want the return to be keys to the empty
+            "items_to_pack" list we made
+
+    items_to_pack.append(my_data[i][0])
+    """
+    
+    ##### my code #####
+    
+    data = [i for i in probData]
+    
+    
+    ###################
+    
         
-    item_keys = [k for k in items.keys()]
-    pack_item = item_keys[0]
-    items_to_pack.append(pack_item)
-    load += items[pack_item][0]
-    value += items[pack_item][1]
+   
     
     return myUsername, nickname, items_to_pack       # use this return statement when you have items to load in the knapsack
-        
+
+
+## try it ####
+
+f1 = open('knapsack.json', 'r')
+data = json.load(f1)
+f1.close()
+
+loadKnapsack(data, 5)
+
+
+##############
+
 
 """ Main code """
 """ Get data and define problem ids """
@@ -66,11 +102,21 @@ probData = getData()
 problems = range(len(probData))
 silent_mode = False    # use this variable to turn on/off appropriate messaging depending on student or instructor use
 """ Error Messages """
-error_bad_list_key = """ 
-A list was received from load_knapsack() for the item numbers to be loaded into the knapsack.  However, that list contained an element that was not a key in the dictionary of the items that were not yet loaded.   This could be either because the element was non-numeric, it was a key that was already loaded into the knapsack, or it was a numeric value that didn't match with any of the dictionary keys. Please check the list that your load_knapsack function is returning. It will be assumed that the knapsack is fully loaded with any items that may have already been loaded and a score computed accordingly. 
+error_bad_list_key = []
+
+""" A list was received from load_knapsack() for the item numbers to be loaded
+    into the knapsack.  However, that list contained an element that was not a key
+    in the dictionary of the items that were not yet loaded.   This could be either
+    because the element was non-numeric, it was a key that was already loaded into
+    the knapsack, or it was a numeric value that didn't match with any of the dictionary keys.
+    Please check the list that your load_knapsack function is returning. It will be assumed
+    that the knapsack is fully loaded with any items that may have already been loaded and a
+    score computed accordingly. 
 """
-error_response_not_list = """
-load_knapsack() returned a response for items to be packed that was not a list.  Scoring will be terminated   """
+error_response_not_list = []
+
+""" load_knapsack() returned a response for items to be packed that was not a list. 
+    Scoring will be terminated   """
 
 for problem_id in problems:
     in_knapsack = {}
